c17f7da2af556fc1f75ab4592ce1d7d67be103fc
GhelbyMF <101785918+GhelbyMF@users.noreply.github.com>
2023-06-22 10:05:22 +0700

    Update get_hycom_3hr.m

--- get_hycom_3hr.m
+++ get_hycom_3hr.m
@@ -8,28 +8,6 @@
 % Greg Pelletier (gjpelletier@gmail.com) (https://github.com/gjpelletier/get_hycom)
 % ----------
 
-% - - -
-% INSTRUCTIONS
-%
-% 1) In the user input section, specify the following:
-% 		- specify the list of variables to be extracted from any combination of var_list = 'surf_el,water_temp,salinity,water_u,water_v'
-% 		- specify the west, east, south, and north extent of the bounding box to be extracted
-%  		- specify the name of the resultDirectory where the hycom data will be saved as output
-% 		- specify the date_start and date_end of the period to be extracted, and the corresponding hycom code for glb and expt
-% 2) Run this script in matlab
-% 3) During execution you sould see the progress of each 3-hour file that is extracted during the period of interest 
-%    from beginning to end. Each nc file name has the format yyyyMMdd_HH.nc to indicate the datetime stamp in UTC
-%
-% Occasionaly there may be times when the hycom server is not responsive 
-% and the script may try several times to download a particular 3-hour time.
-% Each 3-hour time is saved in a separate netcdf file in the specified output folder. 
-% Sometimes if the hycom data are missing there may appear to be an endless loop of trials to download the missing data.
-% In those cases it may be necessary to stop the script and change the date_start to a value that resumes with non-missing data
-% and then re-run the script with that new start time.
-
-% ----------
-
-% ----------
 % ----------
 % USER INPUT SECTION
 
@@ -45,7 +23,7 @@ east = 160;       % 0 to 360 degE
 
 % - - -
 % specify the directory where the extracted nc files will be saved:
-resultDirectory = 'G:\1_Ghelby\Skripsi\Ocean_Parcels\hycom\data\';         % include the ending '\
+resultDirectory = 'G:\hycom\data\';         % include the ending '\
 
 % -  
 % Specify the glb, expt, date_start, and number_of_days up to one year at a time between 1994 to present
@@ -53,7 +31,7 @@ glb = 'GLBy0.08';                        % code for the HYCOM grid that (see the
 expt = '93.0';                           % code for the HYCOM experiment (see the note below for guidance)
 date_start = datetime(2021,12,12,21,0,0);  % starting datetime (starting hour must be either 0, 3, 6, 9, 12, 15, 18, or 21, and must be 12 if the date_start is Jan 1 of the year or first day of the expt. The date_start must be within the range of dates for the glb and expt as described at www.hycom.org)
 date_end = datetime(2022,1,1,9,0,0);  % ending datetime (hour must be either 0, 3, 6, 9, 12, 15, 18, or 21, and must be 21 if the date_end is Dec 31 of the year or last day of the expt. The date_end must be within the range of dates for the glb and expt as described at www.hycom.org)
-% datetime(2022,12,31,21,0,0)
+
 % IMPORTANT: Note for selecting the correct glb and expt for the dates to be downloaded (more info on the glb and expt is available at www.hycom.org if needed):
 % Use glb = 'GLBv0.08' and expt = '53.X' for dates between 1994-2015
 % Use glb = 'GLBv0.08' and expt = '56.3' for dates between 1/1/2016 or 7/1/2014 to 4/30/2016
